19280744fe749e3fbaf07b4bc9a9d97c4f6aa5f6
Zakis0 <65608327+Zakis0@users.noreply.github.com>
2020-11-11 16:04:28 +1000

    Update urls.py

--- ExpertsCalculator/urls.py
+++ ExpertsCalculator/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('Less/', include('Less.urls')),
     path('Score/', include('Score.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
