4d57b805cd45368db97cbf0f64cbb8f3445c42c2
Zakis0 <65608327+Zakis0@users.noreply.github.com>
2020-11-11 13:35:11 +1000

    Update settings.py

--- ExpertsCalculator/settings.py
+++ ExpertsCalculator/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_ROOT = 'static'
 WSGI_APPLICATION = 'ExpertsCalculator.wsgi.application'
